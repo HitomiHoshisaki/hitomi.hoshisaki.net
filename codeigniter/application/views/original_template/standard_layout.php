2de86f3a6e2be2720e9ea08edb6b8f14e430f0b9
ほしさきひとみ <hitomi.hoshisaki+c9@gmail.com>
2018-01-28 14:53:45 +0000

    topページを追加

---
+++ codeigniter/application/views/original_template/standard_layout.php
@@ -0,0 +1,22 @@
+<! DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8" />
+	<title>スタンダードレイアウト</title>
+	<link rel="stylesheet" type="text/css" href="/common/css/import.css" />
+</head>
+<body>
+	<header id="l-header">
+		
+	</header>
+	<nav id="l-nav">
+		
+	</nav>
+	<main id="l-main">
+		
+	</main>
+	<footer id="l-footer">
+		
+	</footer>
+</body>
+</html>
---
+++ codeigniter/application/views/welcome/index.php
@@ -0,0 +1,30 @@
+<! DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8" />
+	<title>ほしさきひとみポートフォリオ</title>
+	<link rel="stylesheet" type="text/css" href="/common/css/import.css" />
+</head>
+<body>
+	
+	<header id="l-header">
+		<span class="l-header-role">WEBエンジニア</span>
+		<span class="l-header-name">ほしさき　ひとみ</span>
+		<span class="l-header-roman">Hitomi Hoshisaki</span>
+	</header>
+	
+	<nav id="l-nav">
+		
+		
+	</nav>
+	
+	<main id="l-main">
+		
+	</main>
+	
+	<footer id="l-footer">
+		
+	</footer>
+
+</body>
+</html>
